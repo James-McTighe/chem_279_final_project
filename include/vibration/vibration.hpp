732b3c73579eaa46277179b754fb5cb387454ba2
James McTighe <james.mctighe@lanelink.com>
2025-12-06 03:54:02 +0000

    initial draft of second derivative

--- include/vibration/vibration.hpp
+++ include/vibration/vibration.hpp
@@ -7,6 +7,7 @@
 #include "atom.hpp"
 #include "fock.hpp"
 #include "gradient.hpp"
+#include "hamiltonian.hpp"
 
 // iteratively updates all atom positions and computes the second direvative for
 // each using central difference
@@ -21,19 +22,63 @@ arma::mat double_central_derivative_approx(std::vector<Atom> atoms, int n_alpha,
     {
         for ( int j = 0; j < 3; ++j )
         {
+            // initial
+            std::vector<Atom> initial = atoms;
+            std::vector<ContractedGaussian> initial_basis =
+                make_sto3g_basis_from_xyz(initial);
+
+            arma::mat initial_ham = core_hamiltonian(initial_basis, initial);
+            SCFState initial_state =
+                solve_SCF_UHF(initial_basis, initial, n_alpha, n_beta);
+            double initial_alpha = calc_electronic_energy(
+                initial_ham, initial_state.F_alpha, initial_state.P_alpha);
+            double initial_beta = calc_electronic_energy(
+                initial_ham, initial_state.F_beta, initial_state.P_beta);
+            double initial_nuclear = nuclear_repulsion(initial);
+
+            double initial_energy =
+                initial_alpha + initial_beta + initial_nuclear;
+
             // forward
             std::vector<Atom> forward = atoms;
             forward[i].pos[j] += step_size;
             std::vector<ContractedGaussian> forward_basis =
                 make_sto3g_basis_from_xyz(forward);
 
+            arma::mat forward_ham = core_hamiltonian(forward_basis, forward);
+            SCFState forward_state =
+                solve_SCF_UHF(forward_basis, forward, n_alpha, n_beta);
+            double forward_alpha = calc_electronic_energy(
+                forward_ham, forward_state.F_alpha, forward_state.P_alpha);
+            double forward_beta = calc_electronic_energy(
+                forward_ham, forward_state.F_beta, forward_state.P_beta);
+            double forward_nuclear = nuclear_repulsion(forward);
+
+            double forward_energy =
+                forward_alpha + forward_beta + forward_nuclear;
 
             // backwards
             std::vector<Atom> backward = atoms;
             backward[i].pos[j] -= step_size;
             std::vector<ContractedGaussian> backward_basis =
                 make_sto3g_basis_from_xyz(backward);
+
+            arma::mat backward_ham = core_hamiltonian(backward_basis, backward);
+            SCFState backward_state =
+                solve_SCF_UHF(backward_basis, backward, n_alpha, n_beta);
+            double backward_alpha = calc_electronic_energy(
+                backward_ham, backward_state.F_alpha, backward_state.P_alpha);
+            double backward_beta = calc_electronic_energy(
+                backward_ham, backward_state.F_beta, backward_state.P_beta);
+            double backward_nuclear = nuclear_repulsion(backward);
+
+            double backward_energy =
+                backward_alpha + backward_beta + backward_nuclear;
+
             // Update second derivative
+            double second_dir_element =
+                (forward_energy - 2 * initial_energy + backward_energy) /
+                (step_size * step_size);
         }
     }
 
